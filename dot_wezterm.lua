c320b386b9646e7cd3d03f96ea582d55baafe826
euisungkim <danielkim.learns@gmail.com>
2025-12-08 21:59:34 -0500

    Update .wezterm.lua

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -8,7 +8,7 @@ local config = wezterm.config_builder()
 
 -- Changing the font size and color scheme.
 config.font_size = 10
-config.color_scheme = 'AdventureTime'
+config.color_scheme = 'Tokyo Night'
 
 -- Hide tabs
 config.hide_tab_bar_if_only_one_tab = true
