4eba8a69473e79ff790e616bf8a39b1821b44a35
euisungkim <danielkim.learns@gmail.com>
2025-12-08 20:23:03 -0500

    Add .wezterm.lua

---
+++ dot_wezterm.lua
@@ -0,0 +1,17 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- This is where you actually apply your config choices.
+
+-- Changing the font size and color scheme.
+config.font_size = 10
+config.color_scheme = 'AdventureTime'
+
+-- Hide tabs
+config.hide_tab_bar_if_only_one_tab = true
+
+-- Finally, return the configuration to wezterm:
+return config
